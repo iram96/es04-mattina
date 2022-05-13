d15f05f89f948f84fb1736e68e8a8cd62b684759
iram96 <mariano.maselli@virgilio.it>
2022-05-13 14:02:54 +0200

    Aggiungere i file di progetto.

---
+++ CodeFile1.cs
@@ -0,0 +1,54 @@
+﻿namespace MyApp
+{
+    public class Student
+    {
+        //Proprietà (privata)
+        private string _myPropertyInternalMatricolaStudente;
+
+        public string MyPropertyPublicMatricolaStudente
+        {
+            get
+            {
+                return _myPropertyInternalMatricolaStudente.Substring(5);
+            }
+
+            set
+            {
+                if (Age < 20)
+                    _myPropertyInternalMatricolaStudente = "00021" + this.StudentName.Substring(0, 5) + value;
+                // value è l'argomento della set
+                else
+                    _myPropertyInternalMatricolaStudente = "00022" + this.StudentName.Substring(0, 5) + value;
+            }
+        }
+
+
+        //Esempi di Auto-implemented Property
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+
+    class EsempioUsoClassStudent
+    {
+        public void EsempioUsoClassStudent_Main()
+        {
+            Student std = new Student()
+            {
+                StudentID = 1,
+                StudentName = "Mariano",
+                Age = 20,
+                Address = "New York",
+                MyPropertyPublicMatricolaStudente = "200"
+            };
+
+            int iStudentID = std.StudentID;
+            string sAppo = std.MyPropertyPublicMatricolaStudente;
+
+            Console.WriteLine("ciao");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+A partire dalla versione C# 3.0 (.NET 3.5) è stata introdotta la Object Initializer Syntax, 
+un nuovo modo di inizializzare un oggetto di una classe o di una collection 
+Questo nuovo modo ti permette di assegnare valori agli attributi al momento della 
+creazione dell'istanza e senza invocare il costruttore.
+
+Da notare la proprietà MATRICOLA dello studente (_myPropertyInternalMatricolaStudente)
+Di questa variabile una parte la voglio tenere interna all'università (es. i primi 5 bytes)
+mentre una parte (i secondi 5 bytes) la voglio rendere publica inclusi i tre bytes finali.
+
+Esercizio: gestire il fatto che se StudentName non è impostato, allora non potrei impostare
+tale proprietà. Allora metto i secondi 5 bytes a 0. E la set di StudentName completa l'operazione.
+
+*/
+
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Studente
+    {
+        private string sMatricolaStudente;
+        //private string? quando nel costruttore è sMatri... = null;
+        public string sCognomeStudente;
+
+        public Studente()
+        {
+            this.sMatricolaStudente = null; //non occupa spazio di memoria
+            sMatricolaStudente = "";
+            sCognomeStudente = "";
+        }
+
+        public void SetMatricolaStudente(string sIdStudente)
+        {
+            this.sMatricolaStudente = "001" + sIdStudente;
+        }
+    }
+
+
+    public class Contenuto
+    {
+        private int iDurataPausa;
+        protected int DurataInMinuti; //accessibile solo dalla classe Serie
+        public string titolo;
+
+        public Contenuto(string titolo)
+        {
+            this.titolo = titolo;
+            this.iDurataPausa = 10;
+        }
+
+        public virtual void Riproduci()
+        {
+            Console.WriteLine("Stai guardando " + titolo);
+        }
+
+    }
+
+    public class Serie : Contenuto
+    {
+        private string[] cast;
+
+        public Serie(string titolo, string[] cast) : base(titolo)
+        {
+            this.cast = cast;
+        }
+
+        public void ModificaDurata(int iNuovaDurata)
+        {
+            this.DurataInMinuti = iNuovaDurata;
+        }
+
+        public override void Riproduci()
+        {
+            Console.WriteLine("Stai guardando la Serie: " + titolo);
+            base.Riproduci();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EsempioUsoClassStudent MioStudente = new EsempioUsoClassStudent();
+            // MioStudente.EsempioUsoClassStudent_Main();
+            string[] MioCast = new string[3];
+            MioCast[0] = "first actor";
+            MioCast[1] = "second actor";
+            MioCast[2] = "third actor";
+            Serie MiaSerie = new Serie("DeadPool", MioCast);
+
+            MiaSerie.titolo = "DeadPool 2";
+            MiaSerie.Riproduci();
+            MiaSerie.ModificaDurata(35);
+
+
+        }
+    }
+}
